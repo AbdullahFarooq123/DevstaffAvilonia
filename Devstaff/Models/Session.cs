1d2a803e7b85082bf85e5a08e1f70411d54b137e
KHAN\Dev <bilal@reborncode.com>
2023-12-09 20:26:34 +0500

    Refactor Errors Fixed

--- Devstaff/Models/Session.cs
+++ Devstaff/Models/Session.cs
@@ -6,14 +6,14 @@ namespace DevStaff.Models;
 
 public static class Session
 {
-    public static string OrganizationName { get; set; } = "";
-    public static string ProjectSearchString { get; set; } = "";
-    public static string TimerLimits { get; set; } = "No Limits";
-    public static DateTime LastUpdatedAt { get; set; } = DateTime.MinValue;
-    public static TimeSpan TodayTime { get; set; } = TimeSpan.Zero;
-    public static User LoggedInUser { get; set; } = null!;
-    public static ProjectUi? SelectedProject { get; set; }
-    public static UserActivity? UserActivity { get; set; }
-    public static List<Screenshot> Screenshots { get; set; } = [];
-    public static List<ProjectUi> Projects { get; set; } = [];
+	public static string OrganizationName { get; set; } = "";
+	public static string ProjectSearchString { get; set; } = "";
+	public static string TimerLimits { get; set; } = "No Limits";
+	public static DateTime LastUpdatedAt { get; set; } = DateTime.MinValue;
+	public static TimeSpan TodayTime { get; set; } = TimeSpan.Zero;
+	public static User LoggedInUser { get; set; } = null!;
+	public static ProjectUi? SelectedProject { get; set; }
+	public static UserActivity? UserActivity { get; set; }
+	public static List<Screenshot> Screenshots { get; set; } = new List<Screenshot>();
+	public static List<ProjectUi> Projects { get; set; } = new List<ProjectUi>();
 }
